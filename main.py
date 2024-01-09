f6bae6473a49585f38de424857ef59abdc056836
Samarth <131678945+Sohm14@users.noreply.github.com>
2024-01-10 00:24:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import os
 import streamlit as st
-from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader, LLMPredictor, PromptHelper, ServiceContext
-from langchain.llms.openai import OpenAI
+from llama_index import VectorStoreIndex, SimpleDirectoryReader, PromptHelper, ServiceContext, StorageContext, \
+    get_response_synthesizer
+from langchain.llms import OpenAI
 from dotenv import load_dotenv
+from llama_index.retrievers import VectorIndexRetriever
+from llama_index.query_engine import RetrieverQueryEngine
+from llama_index.postprocessor import SimilarityPostprocessor
 
 load_dotenv()
 
@@ -10,44 +14,58 @@ openai_api_key = os.getenv('OPENAI_API_KEY')
 
 uploaded_files = st.file_uploader(
     label="#### Upload Your Data File",
-    type=["pdf"],
+    type=["pdf", "txt"],
     key="file_uploader",
     accept_multiple_files=True
 )
 
 
-def get_response(query, openai_api_key):
-    llm_predictor = LLMPredictor(
-        llm=OpenAI(openai_api_key=openai_api_key, temperature=0, model_name="text-davinci-003", max_tokens=512))
-
-    # Configure prompt parameters and initialise helper
-    max_input_size = 4096
-    num_output = 256
-    max_chunk_overlap = 20
-
-    prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap)
-
+def load_document(file_path):
+    llm = OpenAI(openai_api_key=openai_api_key, temperature=0, model_name="gpt-3.5-turbo", max_tokens=512)
     if uploaded_files:
         for uploaded_file in uploaded_files:
+            if not os.path.exists("files"):
+                os.makedirs("files")
             file_path = os.path.join("files", uploaded_file.name)
             with open(file_path, "wb") as f:
                 f.write(uploaded_file.getvalue())
+    documents = SimpleDirectoryReader('./files').load_data()
+    service_context = ServiceContext.from_defaults(llm=llm)
+    index = VectorStoreIndex.from_documents(documents, service_context=service_context)
+    retriever = VectorIndexRetriever(
+        index=index,
+        similarity_top_k=10,
+    )
 
-        documents = SimpleDirectoryReader('./files').load_data()
-        # using openai as the LLM
-        service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor,prompt_helper=prompt_helper)
-        index = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
-        response = index.query(query)
-        for uploaded_file in uploaded_files:
-            file_path = os.path.join("files", uploaded_file.name)
-            os.remove(file_path)
-        if response is None:
-            st.error(f"Oops! No result found ")
-        else:
-            st.success(f"\nBot says :\n\n{response.response}\n\n\n")
+    # configure response synthesizer
+    response_synthesizer = get_response_synthesizer()
 
+    # assemble query engine
+    st.session_state.query_engine = RetrieverQueryEngine(
+        retriever=retriever,
+        response_synthesizer=response_synthesizer,
+        node_postprocessors=[SimilarityPostprocessor(similarity_cutoff=0.7)],
+    )
+
+
+def get_response(query):
+    query_engine = st.session_state.query_engine
+    response = query_engine.query(query)
+    for uploaded_file in uploaded_files:
+        file_path = os.path.join("files", uploaded_file.name)
+        os.remove(file_path)
+
+    if response is None:
+        st.error(f"Oops! No result found ")
+    else:
+        st.success(f"\nBot says :\n\n{response.response}\n\n\n")
+
+
+if st.button('Load'):
+    load_document('./files')
 
 st.title("Chat PDF")
+
 query = st.text_input("What would you like to ask?", "")
 query = query + " based on documents uploaded"
 
@@ -57,8 +75,11 @@ if st.button("Submit"):
     else:
         try:
             if len(openai_api_key) > 0:
-                get_response(query, openai_api_key)
+                if st.session_state.query_engine is not None:
+                    get_response(query)
+                else:
+                    st.error("Please load the documents before submitting the query.")
             else:
-                st.error(f"Enter a valid openai key")
+                st.error("Enter a valid openai key")
         except Exception as e:
             st.error(f"An error occurred: {e}")
